62b713cdffe58a5d17c79b4f46723f097bd6b26c
bpagbla <pagesbeatriz@gmail.com>
2024-11-29 18:38:49 +0100

    base de vista y controlador

--- controlador.php
+++ controlador.php
@@ -1,1 +1,19 @@
 <?php
+
+//require_once "modelo.php";
+require_once "vista.php";
+
+class Controlador{
+    public function __construct(){
+
+    }
+
+    public function inicia(){
+        Vista::MuestraLogin();
+    }
+
+}
+
+$programa = new Controlador();
+
+$programa->inicia();
--- vista.php
+++ vista.php
@@ -1,1 +1,15 @@
 <?php
+
+class Vista{
+    public static function MuestraLogin() {
+/*         echo "Bienvenido a mi página<br><br>
+        
+        <form method='post' action=''>
+            <input type='submit' name='boton' value='entrar'>
+            </form>
+        "; */
+
+
+        include "./frm/login.php";
+    }
+}
