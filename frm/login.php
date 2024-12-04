50f0871dd789eef27d95d9d143e179421de73336
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-04 19:05:45 +0100

    Cambiado diseño login

--- frm/login.php
+++ frm/login.php
@@ -10,8 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css\login.css">
     <?php
 
@@ -21,10 +20,10 @@ session_start();
         include "BD/baseDeDatos.php";
         $ddbb = new BaseDeDatos;
         $ddbb->conectar();
-        $id = $_POST["id"];
-        $password = $_POST["passwd"];
+        //$id = $_POST["id"];
+        //$password = $_POST["passwd"];
 
-        $consulta = $ddbb->consulta("SELECT * FROM `usuario` WHERE EMAIL='$id' || NICK='$id'");
+        //$consulta = $ddbb->consulta("SELECT * FROM `usuario` WHERE EMAIL='$id' || NICK='$id'");
 
     }
 
@@ -34,44 +33,29 @@ session_start();
 
     ?>
 </head>
-
 <body>
-
-    <h1>Inicia Sesión</h1>
-    <div class="consoleBg">
-        <form action="" method="post">
-            <div class="form-row align-items-center">
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInput">Email/Username</label>
-                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="email/username"
-                        name="id">
-                </div>
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInputGroup">Password</label>
-                    <div class="input-group mb-2">
-                        <input type="password" class="form-control" id="inlineFormInputGroup" placeholder="password"
-                            name="passwd">
+    <div class="container-lg">
+        <div class="row justify-content-center align-content-center" style="height: 100vh;">
+            <div class="" ></div>
+            <div class="col-auto">
+                <div class="p-4 rounded-5 shadow-lg">
+                    <div class="row mb-2">
+                        <div class="col-12">
+                            <label for="user" class="form-label">Usuario</label>
+                            <input name="user" type="text" class="form-control" id="user" placeholder="Paco">
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-12">
+                            <label for="passwd" class="form-label">Contraseña</label>
+                            <input name="passwd" type="text" class="form-control" id="passwd" placeholder="1234">
+                        </div>
                     </div>
                 </div>
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-primary mb-2">Login</button>
-                </div>
             </div>
-        </form>
+        </div>
     </div>
-
-
-
-    <!-- Por si necesitamos JavaScript ????? -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
