1b302c0ebea09dcbacbbb5671f10dfe78877ac0e
bpagbla <pagesbeatriz@gmail.com>
2024-12-04 16:35:09 +0100

    login

--- frm/login.php
+++ frm/login.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,24 +12,84 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css\login.css">
+    <?php
+
+    function verificarUsuario()
+    {
+
+        include "../BD/baseDeDatos.php";
+        $ddbb = new BaseDeDatos;
+        $ddbb->conectar();
+
+        $id = $_POST["id"];
+        $password = $_POST["passwd"];
+
+        $ddbb->consulta("SELECT * FROM `usuario` WHERE EMAIL='$id' || NICK='$id' && PASSWORD='$password'");
+
+    }
+
+
+    ?>
 </head>
 
 <body>
 
-<?php
-include "BD/baseDedatos.php";
-$ddbb = new BaseDeDatos;
-$ddbb->conectar();
-$ddbb->consulta("SELECT * FROM usuario");
-?>
+    <?php
+    /* include "BD/baseDedatos.php";
+    $ddbb = new BaseDeDatos;
+    $ddbb->conectar();
+    $ddbb->consulta("SELECT * FROM usuario"); */
+    ?>
 
     <h1>Inicia Sesión</h1>
-    <form action="" method="POST">
+    <div class="consoleBg">
+        <form action="" method="post">
+            <div class="form-row align-items-center">
+                <div class="col-auto">
+                    <label class="sr-only" for="inlineFormInput">Email/Username</label>
+                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="email/username"
+                        name="id">
+                </div>
+                <div class="col-auto">
+                    <label class="sr-only" for="inlineFormInputGroup">Password</label>
+                    <div class="input-group mb-2">
+                        <input type="password" class="form-control" id="inlineFormInputGroup" placeholder="password"
+                            name="passwd">
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary mb-2">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
-        Nick o Correo: <input type="text" name="nick">
-        Password: <input type="password" name="password">
 
-    </form>
+    <?php
+    if (isset($_POST["id"]) && isset($_POST["passwd"])) {
+
+        verificarUsuario();
+
+    }
+
+
+    ?>
+
+
+
+    <!-- Por si necesitamos JavaScript ????? -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
