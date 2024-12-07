8c092c18bd5edadbd6e1bbd7e1608bfa1b1c0ebd
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-07 22:54:38 +0100

    Menu Hamburguesa y principio de sacar juegos

--- model/principal.php
+++ model/principal.php
@@ -1,1 +1,11 @@
 <?php
+
+    function getGames() {
+
+        include_once "../BD/baseDeDatos.php";
+        $ddbb = new BaseDeDatos;
+        $ddbb->conectar();
+        $consulta = $ddbb->consulta("SELECT ID FROM `usuario` WHERE email='$email'");
+
+
+    }
