f3a538c5de2a461266ee90e806f7e6525fa39cf8
bpagbla <pagesbeatriz@gmail.com>
2024-12-07 23:31:39 +0100

    intento de cambiar apariencia de juegos

--- vista/principal.php
+++ vista/principal.php
@@ -113,7 +113,7 @@
         </symbol>
     </svg>
 
-
+    <div class="bg-image"></div>
 
     <div class="container-fluid">
         <div class="row">
@@ -200,10 +200,8 @@
                 </div>
             </div>
 
-
-
             <div class="col">
-                
+
                 <div class="row justify-left">
                     <h2 class="mt-3">Tus Juegos</h2>
                     <div class="col-auto py-3">
@@ -275,8 +273,6 @@
             </div>
         </div>
     </div>
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
