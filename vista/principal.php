59be90c750fea98e452bdb5ea3e1e894e2800823
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-08 18:24:56 +0100

    Añadido estilos a topbar/offcanvas

--- vista/principal.php
+++ vista/principal.php
@@ -114,49 +114,105 @@
     </svg>
 
 
-    <header class="navbar bg-body-tertiary sticky-top d-md-none">
+    <header class="navbar sticky-top d-md-none header-color">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Offcanvas navbar</a>
+            <a class="navbar-brand nombre text-white" href="#">Glitched Pixel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+            <div class="offcanvas offcanvas-end sidebar" tabindex="-1" id="offcanvasNavbar"
                 aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                    <h5 class="offcanvas-title text-white" id="offcanvasNavbarLabel">Menu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
+                            <a href="#" class="nav-link active ps-2" aria-current="page">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#home" />
+                                </svg>
+                                Biblioteca
                             </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
                         </li>
+                        <li>
+                            <a href="#" class="nav-link text-white ps-2">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#speedometer2" />
+                                </svg>
+                                Novedades
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white ps-2">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#table" />
+                                </svg>
+                                Ofertas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white ps-2">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#grid" />
+                                </svg>
+                                Todos los juegos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white ps-2">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#question" />
+                                </svg>
+                                Soporte
+                            </a>
+                        </li>
+                        <?php
+                        if($_SESSION["role"]=="admin") {
+                            print "<li>";
+                            print '<a href="#" class="nav-link text-white ps-2">';
+                            print '<svg class="bi me-2" width="16" height="16">';
+                            print '<use xlink:href="#tools" />';
+                            print '</svg>';
+                            print 'Administrar';
+                            print '</a>';
+                            print '</li>';
+                        }
+                        ?>
                     </ul>
+                </div>
+                <div class="offcanvas-bottom p-3 px-4">
+                    <div class="dropup">
+                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                           id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <svg class="bi me-2" width="32" height="32">
+                                <use xlink:href="#people-circle" />
+                            </svg>
+                            <strong><?php echo $_SESSION["nick"] ?></strong>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow bg-menu"
+                            aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="#">Amigos</a></li>
+                            <li><a class="dropdown-item" href="#">Ajustes</a></li>
+                            <li><a class="dropdown-item" href="#">Perfil</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="" method="POST" class="dropdown-item " >
+                                    <input type="submit" value="Cerrar Sesión" name="logout" class="logout">
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </header>
     <div class="container-fluid">
         <div class="row">
-
             <div class='col-auto ps-0 d-none d-md-block'>
                 <div class="d-flex flex-column flex-shrink-0 p-3 text-white sidebar sticky-top">
                     <a href="#"
