bebd5c103dccba06bfec831e28f3591f08e136ed
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 18:45:45 +0100

    Si error saca error

--- vista/login.php
+++ vista/login.php
@@ -1,7 +1,7 @@
 <?php
 include "../frm/login.php";
 
-if ($loginAttempt) { print "<script async src='../js/login.js'></script>"; }
+if ($added) { print "<script async src='../js/login.js'></script>"; }
 
 ?>
 </body>
