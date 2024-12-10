c76665b4f5879c3bd32fa14299acee7747877dea
bpagbla <pagesbeatriz@gmail.com>
2024-12-10 19:05:05 +0100

    mensaje nueva cuenta funciona

--- vista/login.php
+++ vista/login.php
@@ -1,10 +1,16 @@
 <?php
 include "../frm/login.php";
 
-if ($loginAttempt) { print "<script async src='../js/login.js'></script>"; }
+if ($loginAttempt) {
+    print "<script async src='../js/login.js'></script>";
+}
 
-if(isset($_SESSION["nuevaCuenta"])) { print "<script async src='../js/nuevaCuenta.js'></script>"; }
+if (isset($_SESSION["nuevaCuenta"]) && $_SESSION["nuevaCuenta"]==true) {
+    print "<script async src='../js/nuevaCuenta.js'></script>";
+    $_SESSION["nuevaCuenta"]=false;
+}
 
 ?>
 </body>
+
 </html>
