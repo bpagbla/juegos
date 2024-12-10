a6952ea8e14b4b17bc3ecf90aeb91622e6dece00
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 18:44:40 +0100

    Errores salen

--- vista/registro.php
+++ vista/registro.php
@@ -4,6 +4,10 @@ class Vista{
     public static function MuestraRegistro() {
 
         include "../frm/registro.php";
+        if ($loginAttempt) { print "<script async src='../js/login.js'></script>"; }
+        print "</body>";
+        print "</html>";
+
     }
 
 }
