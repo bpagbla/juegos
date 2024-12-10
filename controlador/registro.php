fe1881d000bf645beec8ca87f64648bdcf303453
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 18:13:40 +0100

    usuario

--- controlador/registro.php
+++ controlador/registro.php
@@ -20,7 +20,7 @@ class Controlador
         if (!$added && $allPosts) {
             $error = 'Cuenta no creada porque el email o nick ya se esta usando';
         }
-        echo $erroru
+        echo $error;
         Vista::MuestraRegistro();
     }
 
