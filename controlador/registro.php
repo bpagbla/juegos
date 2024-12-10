d5509d065b30788c9d3b010002c2a516c04eb93b
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 18:10:39 +0100

    añadir usuario

--- controlador/registro.php
+++ controlador/registro.php
@@ -11,7 +11,15 @@ class Controlador
     public function inicia()
     {
         include_once '../controlador/registro.php';
-        anadirUsuario();
+        $added = false;
+        $allPosts = (isset($_POST["email"]) && isset($_POST["nick"]) && isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["pass"]));
+        if ($allPosts) {
+            $added = anadirUsuario($_POST["email"], $_POST["nick"], $_POST["nombre"], $_POST["apellidos"], $_POST["pass"]);
+        }
+        $error = '';
+        if (!$added && $allPosts) {
+            $error = 'Cuenta no creada porque el email o nick ya se esta usando';
+        }
         Vista::MuestraRegistro();
     }
 
