88ce525ea067c961630f863dfc7de7b454abc481
bpagbla <pagesbeatriz@gmail.com>
2024-12-10 18:46:22 +0100

    registro redirecciona a login

--- controlador/registro.php
+++ controlador/registro.php
@@ -17,14 +17,15 @@ class Controlador
         include_once '../model/registro.php';
 
         $added = false;
+        $error = '';
         $allPosts = (isset($_POST["email"]) && isset($_POST["nick"]) && isset($_POST["nombre"]) && isset($_POST["apellidos"]) && isset($_POST["passwd"]));
         if ($allPosts) {
             $added = anadirUsuario($_POST["email"], $_POST["nick"], $_POST["nombre"], $_POST["apellidos"], $_POST["passwd"]);
         }
-        $error = '';
-        if (!$added && $allPosts) {
-            echo 'Cuenta no creada porque el email o nick ya se esta usando';
+        if($added){
+            header("Location: login.php");
         }
+
         Vista::MuestraRegistro();
     }
 
