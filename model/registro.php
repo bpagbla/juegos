c50c539acb7c842875cd684ffd0ca45ae37ed6ba
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 17:59:01 +0100

    arreglado redirect

--- model/registro.php
+++ model/registro.php
@@ -7,7 +7,9 @@ function verificarUsuario($id, $password)
     $ddbb = new BaseDeDatos;
     $ddbb->conectar();
 
-    $consulta = $ddbb->consulta("INSERT INTO usuario(EMAIL, NICK, NOMBRE, APELLIDOS, PASSWORD, ROLE) VALUES($email,$nick,$nombre,$apel,$pass,'user'");
+    $pass = password_hash($pass);
+
+    $consulta = $ddbb->consulta("INSERT INTO usuario(EMAIL, NICK, NOMBRE, APELLIDOS, PASSWORD, ROLE) VALUES('$email','$nick','$nombre','$apel','$pass','user'");
 
 
 }
