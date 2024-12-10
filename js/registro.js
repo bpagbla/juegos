ca656d89e4d399ad4bcd512bc41a78a376d18e6e
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 19:02:39 +0100

    Registro da error al tener email duplicado

--- js/registro.js
+++ js/registro.js
@@ -16,8 +16,3 @@
         }, false)
     })
 })()
-
-const toastLiveExample = document.getElementById('liveToast')
-
-const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-toastBootstrap.show()
