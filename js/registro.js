c12fe4c57285bad449e66684d6a6c2474fc9388d
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 18:54:35 +0100

    Cambios registro

--- js/registro.js
+++ js/registro.js
@@ -16,3 +16,8 @@
         }, false)
     })
 })()
+
+const toastLiveExample = document.getElementById('liveToast')
+
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastBootstrap.show()
--- vista/registro.php
+++ vista/registro.php
@@ -4,7 +4,7 @@ class Vista{
     public static function MuestraRegistro() {
 
         include "../frm/registro.php";
-        if ($loginAttempt) { print "<script async src='../js/login.js'></script>"; }
+        if ($added) { print "<script async src='../js/login.js'></script>"; }
         print "</body>";
         print "</html>";
 
