177d3951ae24fa44db6c925e53e66b9cd3154176
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-10 17:55:54 +0100

    registro

--- model/registro.php
+++ model/registro.php
@@ -7,32 +7,7 @@ function verificarUsuario($id, $password)
     $ddbb = new BaseDeDatos;
     $ddbb->conectar();
 
-    $consulta = $ddbb->consulta("SELECT * FROM `usuario` WHERE EMAIL='$id' || NICK='$id'");
-    if (!empty($consulta)) {
-        $consPass = $ddbb->consulta("SELECT password FROM `usuario` WHERE EMAIL='$id' || NICK='$id'");
-        $passReal = "";
-        foreach ($consPass as $row) {
-            $passReal = $row["password"];
-        }
+    $consulta = $ddbb->consulta("INSERT INTO usuario(EMAIL, NICK, NOMBRE, APELLIDOS, PASSWORD, ROLE) VALUES($email,$nick,$nombre,$apel,$pass,'user'");
 
-        if (password_verify($password, $passReal)) {
-            $ddbb->prepare();
-            $ddbb->bindParam(':id',$id);
-            $datos = $ddbb->execute();
-            $ddbb->cerrar();
-            foreach ($datos as $row) {
-                $_SESSION["nick"] = $row["nick"];
-                $_SESSION["email"] = $row["email"];
-                $_SESSION["id"] = $row["id"];
-                $_SESSION["role"] = $row["role"];
-            }
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    return false;
 
 }
