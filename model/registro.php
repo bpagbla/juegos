a3c420fab43383dc9fece3a9dbea4c9b1ebd2685
bpagbla <pagesbeatriz@gmail.com>
2024-12-11 18:17:32 +0100

    comprobar contraseñas

--- model/registro.php
+++ model/registro.php
@@ -16,14 +16,25 @@ function anadirUsuario($email, $nick, $nombre, $apel, $pass)
             $existe = true; //si están duplicados se cambia la variable a true
         }
     }
+
     if (!$existe) { //si la variable es false se crea el usuario
 
         $pass = password_hash($pass, PASSWORD_DEFAULT); //se convierte a hash la contraseña
 
         //se insertan los datos en la base de detos
-        $consulta = $ddbb->insert("INSERT INTO usuario(EMAIL, NICK, NOMBRE, APELLIDOS, PASSWORD, ROLE) VALUES(?,?,?,?,?,?)",[$email,$nick,$nombre,$apel,$pass,'user']);
+        $consulta = $ddbb->insert("INSERT INTO usuario(EMAIL, NICK, NOMBRE, APELLIDOS, PASSWORD, ROLE) VALUES(?,?,?,?,?,?)", [$email,$nick,$nombre,$apel,$pass,'user']);
         return true; //devuelve true si se ha creado
     } else {
         return false; //devuelve false si no se ha creado
+    }
+}
+
+function comprobarPasswd(){
+    if(isset($_POST["passwd"]) && isset($_POST["passwd2"])){
+        if($_POST["passwd"] == $_POST["passwd2"]){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
