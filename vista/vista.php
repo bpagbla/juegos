fd086e40a77231c8c9f84321b65d253ba7bc2a02
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-13 18:50:59 +0100

    Movido controlador

--- vista/vista.php
+++ vista/vista.php
@@ -4,20 +4,20 @@ class Vista
 {
     public static function mostrarLanding(): void
     {
-        include_once "../frm/landing.php";
+        include_once "frm/landing.php";
     }
 
     public static function mostrarLogin($loginAttempt): void
     {
-        include_once "../frm/login.php";
+        include_once "frm/login.php";
     }
 
     public static function mostrarPrincipal($games) {
-        include_once "../frm/principal.php";
+        include_once "frm/principal.php";
     }
 
     public static function mostrarRegistro($allPosts,$added,$passwdBien) {
-        include_once "../frm/registro.php";
+        include_once "frm/registro.php";
     }
 
 }
