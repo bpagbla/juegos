9b2df4276fbbae5dab5b89306f9dc53c441de8a2
bpagbla <pagesbeatriz@gmail.com>
2024-12-19 14:07:02 +0100

    sidebar separado

--- frm/admin.php
+++ frm/admin.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glitched Pixel | Administrar</title>
+
+</head>
+
+<body>
+    <!-- SIDEBAR -->
+    <?php
+        include('frm/templates/sidebar.php');
+    ?>
+    <!-- CONTENIDO -->
+
+</body>
+
+</html>
--- frm/landing.php
+++ frm/landing.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Glitched Pixel</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbar-fixed/">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
