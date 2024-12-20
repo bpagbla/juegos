c72e6ee47c048f0f36eaf8b5554ebace4c8e8a89
bpagbla <pagesbeatriz@gmail.com>
2024-12-20 23:55:02 +0100

    modal adm juegos

--- frm/adm-juegos.php
+++ frm/adm-juegos.php
@@ -6,9 +6,46 @@
     ?>
 </div>
 
-<button type="button" class="btn text-white position-fixed posFixBotomRight">
+<button type="button" class="btn text-white position-fixed posFixBotomRight" data-bs-toggle="modal"
+    data-bs-target="#exampleModal">
     <svg class="bi me-2" width="16" height="16">
         <use xlink:href="#plus" />
     </svg>
     Añadir
 </button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Añadir un Juego Nuevo</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Título:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Descripción:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
+</script>
