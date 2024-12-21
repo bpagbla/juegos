f7e5e901b7e0d4d02c8b9f0e310443c05d262756
bpagbla <pagesbeatriz@gmail.com>
2024-12-21 20:33:18 +0100

    script para visualizar imagen

--- js/img-selector.js
+++ js/img-selector.js
@@ -1,27 +1,21 @@
 const selectImage = document.querySelector('.select-img');
 const inputFile = document.querySelector('#file');
-const imgArea = document.querySelector('.img-area');
+const portada = document.querySelector('.portada');
 
 selectImage.addEventListener('click', function () {
-	inputFile.click();
-})
+    inputFile.click();
+});
 
 inputFile.addEventListener('change', function () {
-	const image = this.files[0]
-	if(image.size < 2000000) {
-		const reader = new FileReader();
-		reader.onload = ()=> {
-			const allImg = imgArea.querySelectorAll('img');
-			allImg.forEach(item=> item.remove());
-			const imgUrl = reader.result;
-			const img = document.createElement('img');
-			img.src = imgUrl;
-			imgArea.appendChild(img);
-			imgArea.classList.add('active');
-			imgArea.dataset.img = image.name;
-		}
-		reader.readAsDataURL(image);
-	} else {
-		alert("Image size more than 2MB");
-	}
-})
+    const image = this.files[0];
+    if (image.size < 2000000) { // Verifica que el tamaño sea menor a 2MB
+        const reader = new FileReader();
+        reader.onload = () => {
+            const imgUrl = reader.result;
+            portada.src = imgUrl; // Reemplaza la imagen de la clase "portada"
+        };
+        reader.readAsDataURL(image); // Lee el archivo como una URL de datos (base64)
+    } else {
+        alert("El tamaño de la imagen debe ser menor a 2MB");
+    }
+});
