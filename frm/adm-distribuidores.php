485af7b0f73847772b42a596dd35807922dcf65e
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-21 20:24:37 +0100

    Added dev and dist to adm panel

---
+++ frm/adm-distribuidores.php
@@ -0,0 +1,6 @@
+<button type="button" class="btn text-white position-fixed end-0 bottom-0 m-4">
+    <svg class="bi me-2" width="16" height="16">
+        <use xlink:href="#plus" />
+    </svg>
+    Añadir
+</button>
