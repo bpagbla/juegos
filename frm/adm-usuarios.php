ca7c2ed2bca6d6474d8624ff70142e9f8cc1a751
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-22 14:59:50 +0100

    Finished password reset form

--- frm/adm-usuarios.php
+++ frm/adm-usuarios.php
@@ -13,7 +13,7 @@ foreach ($users as $user) {
 
 <?php
 if (isset($_POST["action"]) && $_POST["action"] == "user-edit") {
-?>
+    ?>
 
     <!-- Modal -->
     <div class="modal fade modal-lg" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
@@ -97,6 +97,66 @@ if (isset($_POST["action"]) && $_POST["action"] == "user-edit") {
         myModal.show();
     </script>
 
+    <?php
+}
+?>
+
 <?php
+if (isset($_POST["action"]) && $_POST["action"] == "user-passwd") {
+    ?>
+
+    <!-- Modal -->
+    <div class="modal fade modal-lg" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Restablecer contraseña para <?php echo $_POST["nick"]; ?></h1>
+                    <button type="submit" class="btn-close" form="model-cancel"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="model-cancel" method="post"></form>
+                    <form id="modal-form" method="post">
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="passwd" class="form-label ms-1">Contraseña</label>
+                                <input type="password" class="form-control" id="passwd" placeholder="*************"
+                                       value="<?php if (!empty($_POST["passwd"])) {
+                                           print $_POST["passwd"];
+                                       } ?>" name="passwd" required>
+                                <div class="invalid-feedback">
+                                    Introduce una contraseña valida.
+                                </div>
+                            </div>
+
+                            <div class="col-12 mt-3">
+                                <label for="passwd2" class="form-label ms-1">Repite Contraseña</label>
+                                <input type="password" class="form-control" id="passwd2" placeholder="*************"
+                                       value="<?php if (!empty($_POST["passwd2"])) {
+                                           print $_POST["passwd2"];
+                                       } ?>" name="passwd2" required>
+                                <div class="invalid-feedback">
+                                    Las dos contraseñas no son iguales.
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-secondary" form="model-cancel">Cancelar</button>
+                    <button type="submit" class="btn btn-primary" value="apply" form="modal-form">Aplicar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const myModal = new bootstrap.Modal('#myModal', {
+            keyboard: false
+        })
+        myModal.show();
+    </script>
+
+    <?php
 }
 ?>
