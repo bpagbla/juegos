4ee6327d3f3169198c270d1edb38274cf608b94c
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-26 18:46:47 +0100

    finished obtaining game names

--- frm/api/games.php
+++ frm/api/games.php
@@ -1,7 +1,14 @@
 <?php
     $curl = curl_init();
     $params = array('api_key' => 'moby_Ivjf8fphPEz3gLn9DVIcRsvNYgE');
-    $params['format'] = 'brief';
     $params['limit'] = '5';
+    $params['format'] = $format;
+    $params['title'] = $title;
     curl_setopt($curl, CURLOPT_URL, 'https://games.eduardojaramillo.click/v1/games?'.http_build_query($params));
+    header('Content-Type: application/json; charset=utf-8');
     print_r(curl_exec($curl));
+
+    //$file = fopen('cache/'.$values.'.json', 'w');
+
+
+    curl_close($curl);
