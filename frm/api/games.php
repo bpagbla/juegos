754049dc1876eb0856736f0938c922b28f9ac4bc
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-26 18:47:40 +0100

    fk cache. all my homies hate cache

--- frm/api/games.php
+++ frm/api/games.php
@@ -7,8 +7,4 @@
     curl_setopt($curl, CURLOPT_URL, 'https://games.eduardojaramillo.click/v1/games?'.http_build_query($params));
     header('Content-Type: application/json; charset=utf-8');
     print_r(curl_exec($curl));
-
-    //$file = fopen('cache/'.$values.'.json', 'w');
-
-
     curl_close($curl);
