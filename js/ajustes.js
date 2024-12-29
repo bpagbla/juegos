67fcf7cc1d9d97f3a4ab0a19293bcd880d604958
eduardo-jaramillo <enjs2002@gmail.com>
2024-12-29 20:20:08 +0100

    Añadido efectos submit contraseña

---
+++ js/ajustes.js
@@ -0,0 +1,59 @@
+let passwd1 = document.getElementById("passwd1")
+let passwd2 = document.getElementById("passwd2")
+
+passwd1.addEventListener('input', function() {
+    if (!passwd1.value && !passwd2.value) {
+        passwd1.classList.remove('success-input')
+        passwd2.classList.remove('success-input')
+        passwd1.classList.remove('error-input')
+        passwd2.classList.remove('error-input')
+        return;
+    }
+
+    if (!passwd2.value) {return;}
+
+    if (passwd1.value === passwd2.value) {
+        passwd1.classList.remove('error-input')
+        passwd2.classList.remove('error-input')
+        passwd1.classList.add('success-input')
+        passwd2.classList.add('success-input')
+    } else {
+        passwd1.classList.add('error-input')
+        passwd2.classList.add('error-input')
+        passwd1.classList.remove('success-input')
+        passwd2.classList.remove('success-input')
+    }
+
+})
+
+passwd2.addEventListener('input', function() {
+        if (passwd1.value === passwd2.value) {
+            passwd1.classList.remove('error-input')
+            passwd2.classList.remove('error-input')
+            if (!passwd1.value && !passwd2.value) {
+                passwd1.classList.remove('success-input')
+                passwd2.classList.remove('success-input')
+            } else {
+                passwd1.classList.add('success-input')
+                passwd2.classList.add('success-input')
+            }
+        } else {
+            passwd1.classList.add('error-input')
+            passwd2.classList.add('error-input')
+            passwd1.classList.remove('success-input')
+            passwd2.classList.remove('success-input')
+        }
+})
+
+function validatePasswd() {
+    if (passwd1.value !== passwd2.value || !passwd1.value) {
+        passwd1.classList.add('shake')
+        passwd2.classList.add('shake')
+        setTimeout(function () {
+            passwd1.classList.remove('shake')
+            passwd2.classList.remove('shake')
+        }, 1000)
+        return false;
+    }
+    return true;
+}
