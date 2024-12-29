150098294570ddb51cad9f0c6c1938e850258ab7
bpagbla <pagesbeatriz@gmail.com>
2024-12-29 13:37:35 +0100

    clase carrito

---
+++ clases/carrito.php
@@ -0,0 +1,27 @@
+<?php
+class Carrito
+{
+private $juegos=array();
+
+
+public function cargaCesta($id){
+
+
+}
+
+public function addJuego($idJuego){
+
+
+}
+
+public function guardaCesta($id){
+
+
+}
+
+public function mostrarCesta($id){
+
+
+}
+
+}
