09cae7e119c354bc9077edb31b915777d4324e9c
bpagbla <pagesbeatriz@gmail.com>
2025-01-10 19:47:10 +0100

    modal prestar

--- frm/templates/card-principal.php
+++ frm/templates/card-principal.php
@@ -12,3 +12,30 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="staticBackdrop<?php print $game[0] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Prestar Juego</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post" id="regalarJuego">
+                        <div class="mb-3">
+                            <label for="prestarNickname" class="form-label">¿A quién quieres prestar este juego?</label>
+                            <input type="text" class="form-control" id="prestarNickname" name="prestarNickname<?php print $game[0] ?>" required placeholder="nickname">
+                        </div>
+                        <div class="mb-3">
+                            <input type="hidden" name="idJuegoPrestar<?php print $game[0] ?>">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary"  data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary" form="regalarJuego">Prestar</button>
+                </div>
+            </div>
+        </div>
+    </div>
