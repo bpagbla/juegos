14f3c06908eb41b4e90eb0183d3fb906f3b70810
eduardo-jaramillo <enjs2002@gmail.com>
2025-01-27 15:54:43 +0100

    se puede quitar los generos

--- js/juegos.js
+++ js/juegos.js
@@ -111,3 +111,7 @@ function startQueueGen(e) {
     clearTimeout(timeoutGen)
     timeoutGen = setTimeout(function () { loadNamesGen(e) }, 100);
 }
+
+document.querySelectorAll('.removable-buttons').forEach( (elem) => {
+    elem.addEventListener('click', function (e) { e.target.closest("div").remove() })
+})
