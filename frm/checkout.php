64f000f8e82578ab233b3b73dff917b478b02ba0
eduardo-jaramillo <enjs2002@gmail.com>
2025-02-14 19:03:05 +0100

    radio for cards

--- frm/checkout.php
+++ frm/checkout.php
@@ -118,7 +118,20 @@
 
                     <h4 class="mb-3">Tarjeta de Credito</h4>
 
-                    <div class="row gy-3">
+                    <div class="row border-1 border rounded my-2 mx-1">
+                        <?php
+                        $first = true;
+                        foreach ($cards as $card) {
+                            if ($first) {
+                                $first = false;
+                            } else { ?>
+                                <hr class="my-0">
+                            <?php } ?>
+                            <div class="col-12 align-items-center justify-content-between d-flex py-2">
+                                <p class="m-0"><?php print 'Mastercard:'.$card["num"].' | '.date("m/y",$card["date"]); if ($card['date'] < time()) print ' | <span class="text-danger">Caducado</span>'; ?></p>
+                                <input type="radio" name="card" value="<?php print $card['num'].$card['date'] ?>">
+                            </div>
+                        <?php } ?>
                     </div>
 
                     <hr class="my-4">
