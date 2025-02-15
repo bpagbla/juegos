d33164c9bb1337a938eae55d972c297f4481c4c0
bpagbla <pagesbeatriz@gmail.com>
2025-02-15 18:30:34 +0100

    phpdocs

--- BD/baseDeDatos.php
+++ BD/baseDeDatos.php
@@ -18,7 +18,7 @@ class BaseDeDatos
 
     //funcion para hacer consultas con un query sql
     /**
-     * @param String $sql
+     * @param string $sql
      * @param array $array
      * @return null
      */
@@ -36,6 +36,12 @@ class BaseDeDatos
     }
 
     //funcion para insertar datos en la base de daots
+    /**
+     * @param string $sql
+     * @param array $array
+     * 
+     * @return bool
+     */
     public function insert($sql,$array) {
         $stmt= $this->db->prepare($sql);
         return $stmt->execute($array);
